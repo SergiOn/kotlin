d4f5138bb6ba1f3be5d9b2d0a55c2934a6a9a948
SergiOn <onishhenko@gmail.com>
2018-05-13 15:09:27 +0300

    # microservices # lesson 2 # updated Test

--- smlr/src/test/kotlin/com/finnetrolle/smlr/service/DefaultKeyMapperServiceTest.kt
+++ smlr/src/test/kotlin/com/finnetrolle/smlr/service/DefaultKeyMapperServiceTest.kt
@@ -8,12 +8,17 @@ class DefaultKeyMapperServiceTest {
 
     val service: KeyMapperService = DefaultKeyMapperService()
 
+    private val KEY: String = "aAbBcCdD"
+    private val LINK: String = "http://www.eveonline.com"
+    private val LINK_NEW: String = "http://wow.ru"
+
     @Test
     fun clientCanAddNewKeyWithLink() {
 //        org.junit.Assert.assertEquals(KeyMapperService.Add.Success(KEY, LINK), service.add(KEY, LINK))
         assertEquals(KeyMapperService.Add.Success(KEY, LINK), service.add(KEY, LINK))
         assertEquals(KeyMapperService.Get.Link(LINK), service.getLink(KEY))
     }
+
 
     @Test
     fun clientCanNotAddExistingKey() {
