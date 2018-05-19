1ffcb94df130cfadd912e0e193c10c15af81283d
SergiOn <onishhenko@gmail.com>
2018-05-20 00:14:11 +0300

    # microservices # lesson 3 # wrote tests DefaultKeyMapperServiceTest

--- smlr/src/test/kotlin/com/finnetrolle/smlr/service/DefaultKeyMapperServiceTest.kt
+++ smlr/src/test/kotlin/com/finnetrolle/smlr/service/DefaultKeyMapperServiceTest.kt
@@ -9,28 +9,37 @@ class DefaultKeyMapperServiceTest {
     val service: KeyMapperService = DefaultKeyMapperService()
 
     private val KEY: String = "aAbBcCdD"
-    private val LINK: String = "http://www.eveonline.com"
-    private val LINK_NEW: String = "http://wow.ru"
+    private val LINK_A: String = "http://google.com"
+    private val LINK_B: String = "http://yahoo.com"
 
     @Test
-    fun clientCanAddNewKeyWithLink() {
-//        org.junit.Assert.assertEquals(KeyMapperService.Add.Success(KEY, LINK), service.add(KEY, LINK))
-        assertEquals(KeyMapperService.Add.Success(KEY, LINK), service.add(KEY, LINK))
-        assertEquals(KeyMapperService.Get.Link(LINK), service.getLink(KEY))
-    }
-
-
-    @Test
-    fun clientCanNotAddExistingKey() {
-        service.add(KEY, LINK)
-//        assertEquals(KeyMapperService.Add.AlreadyExist(KEY), service.add(KEY, LINK))
-        assertEquals(KeyMapperService.Add.AlreadyExist(KEY), service.add(KEY, LINK_NEW))
-        assertEquals(KeyMapperService.Get.Link(LINK), service.getLink(KEY))
+    fun clientCanAddLinks() {
+        val keyA = service.add(LINK_A)
+        assertEquals(KeyMapperService.Get.Link(LINK_A), service.getLink(keyA))
+        val keyB = service.add(LINK_B)
+        assertEquals(KeyMapperService.Get.Link(LINK_B), service.getLink(keyB))
     }
 
     @Test
     fun clientCanNotTakeLinkIfKeyIsNotFoundInService() {
         assertEquals(KeyMapperService.Get.NotFound(KEY), service.getLink(KEY))
     }
+
+
+//    @Test
+//    fun clientCanAddNewKeyWithLink() {
+////        org.junit.Assert.assertEquals(KeyMapperService.Add.Success(KEY, LINK), service.add(KEY, LINK))
+//        assertEquals(KeyMapperService.Add.Success(KEY, LINK), service.add(KEY, LINK))
+//        assertEquals(KeyMapperService.Get.Link(LINK), service.getLink(KEY))
+//    }
+
+
+//    @Test
+//    fun clientCanNotAddExistingKey() {
+//        service.add(KEY, LINK)
+////        assertEquals(KeyMapperService.Add.AlreadyExist(KEY), service.add(KEY, LINK))
+//        assertEquals(KeyMapperService.Add.AlreadyExist(KEY), service.add(KEY, LINK_NEW))
+//        assertEquals(KeyMapperService.Get.Link(LINK), service.getLink(KEY))
+//    }
 
 }
