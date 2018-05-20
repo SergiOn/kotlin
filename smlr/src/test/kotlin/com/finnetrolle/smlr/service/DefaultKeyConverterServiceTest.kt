e0c0142c89b81887b8c0e7a0aed8b4d41877bbf6
SergiOn <onishhenko@gmail.com>
2018-05-20 14:13:12 +0300

    # microservices # lesson 3 # implemented DefaultKeyConverterService

--- smlr/src/test/kotlin/com/finnetrolle/smlr/service/DefaultKeyConverterServiceTest.kt
+++ smlr/src/test/kotlin/com/finnetrolle/smlr/service/DefaultKeyConverterServiceTest.kt
@@ -9,7 +9,7 @@ class DefaultKeyConverterServiceTest {
     val service: KeyConverterService = DefaultKeyConverterService()
 
     @Test
-    fun givenIdMustBeConvertableBothWays() {
+    fun givenIdMustBeConvertibleBothWays() {
         val rand = Random()
         for (i in 0..1000L) {
             val initialId = Math.abs(rand.nextLong())
