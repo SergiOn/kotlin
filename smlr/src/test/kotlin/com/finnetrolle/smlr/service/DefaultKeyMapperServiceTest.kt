7ceb1c010051096354b345f800bfe31ff8a2a8c2
SergiOn <onishhenko@gmail.com>
2018-05-20 13:53:10 +0300

    # microservices # lesson 3 # updated DefaultKeyMapperServiceTest

--- smlr/src/test/kotlin/com/finnetrolle/smlr/service/DefaultKeyMapperServiceTest.kt
+++ smlr/src/test/kotlin/com/finnetrolle/smlr/service/DefaultKeyMapperServiceTest.kt
@@ -1,16 +1,40 @@
 package com.finnetrolle.smlr.service
 
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
 
 
 class DefaultKeyMapperServiceTest {
 
+    @InjectMocks
     val service: KeyMapperService = DefaultKeyMapperService()
 
     private val KEY: String = "aAbBcCdD"
     private val LINK_A: String = "http://google.com"
     private val LINK_B: String = "http://yahoo.com"
+
+    @Mock
+    lateinit var converter: KeyConverterService
+
+    private val KEY_A: String = "abc"
+    private val KEY_B: String = "cde"
+    private val ID_A: Long = 10000000L
+    private val ID_B: Long = 10000001L
+
+    @Before
+    fun init() {
+        MockitoAnnotations.initMocks(this)
+
+        Mockito.`when`(converter.keyToId(KEY_A)).thenReturn(ID_A)
+        Mockito.`when`(converter.idToKey(ID_A)).thenReturn(KEY_A)
+        Mockito.`when`(converter.keyToId(KEY_B)).thenReturn(ID_B)
+        Mockito.`when`(converter.idToKey(ID_B)).thenReturn(KEY_B)
+    }
 
     @Test
     fun clientCanAddLinks() {
