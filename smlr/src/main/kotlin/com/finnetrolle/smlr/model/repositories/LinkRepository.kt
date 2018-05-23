740c42b356ba88c4a0f2ccaf756ba6eda8c80f08
SergiOn <onishhenko@gmail.com>
2018-05-24 00:22:19 +0300

    # microservices # lesson 4 # added link to db interface description: https://stackoverflow.com/questions/44101061/missing-crudrepositoryfindone-method

--- smlr/src/main/kotlin/com/finnetrolle/smlr/model/repositories/LinkRepository.kt
+++ smlr/src/main/kotlin/com/finnetrolle/smlr/model/repositories/LinkRepository.kt
@@ -1,6 +1,7 @@
 package com.finnetrolle.smlr.model.repositories
 
 import com.finnetrolle.smlr.model.Link
+//import org.springframework.data.repository.CrudRepository
 //import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.Repository
 import java.util.*
@@ -8,6 +9,7 @@ import java.util.*
 
 
 //interface LinkRepository : JpaRepository<Link, Long> {
+//interface LinkRepository : CrudRepository<Link, Long> {
 interface LinkRepository : Repository<Link, Long> {
 //    fun findOne(id: Long?): Optional<Link>
     fun findById(id: Long?): Optional<Link>
