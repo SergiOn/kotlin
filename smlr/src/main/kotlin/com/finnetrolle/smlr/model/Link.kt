453b9772d2fd4612f29e28be56543f9100bc272b
SergiOn <onishhenko@gmail.com>
2018-05-31 09:04:08 +0300

    # microservices # lesson 5 # connected Table configuration with postgresql

--- smlr/src/main/kotlin/com/finnetrolle/smlr/model/Link.kt
+++ smlr/src/main/kotlin/com/finnetrolle/smlr/model/Link.kt
@@ -1,15 +1,15 @@
 package com.finnetrolle.smlr.model
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
-import javax.persistence.Table
+import javax.persistence.*
 
 
 @Entity
 @Table(name = "links")
 data class Link(
         var text: String = "",
-        @Id @GeneratedValue(strategy = GenerationType.AUTO) var id: Long = 0
+        @Id
+//        @GeneratedValue(strategy = GenerationType.AUTO)
+        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "links_sequence")
+        @SequenceGenerator(name = "links_sequence", sequenceName = "links_seq")
+        var id: Long = 0
 )
