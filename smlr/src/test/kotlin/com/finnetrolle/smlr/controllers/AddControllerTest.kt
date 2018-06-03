5045991f9dab13a2b6883ad84cffa73224481557
SergiOn <onishhenko@gmail.com>
2018-06-03 15:20:36 +0300

    # microservices # lesson 6 # wrote test in AddControllerTest file

--- smlr/src/test/kotlin/com/finnetrolle/smlr/controllers/AddControllerTest.kt
+++ smlr/src/test/kotlin/com/finnetrolle/smlr/controllers/AddControllerTest.kt
@@ -1,20 +1,25 @@
 package com.finnetrolle.smlr.controllers
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.finnetrolle.smlr.SmlrApplication
 import com.finnetrolle.smlr.service.KeyMapperService
 import com.finnetrolle.smlr.whenever
+import org.hamcrest.Matchers
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
-import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
 
@@ -48,6 +53,15 @@ class AddControllerTest {
                 .build()
 
         whenever(service.add(LINK)).thenReturn(KEY)
+    }
+
+    @Test
+    fun whenUserAddLinkHeTakesAKey() {
+        mockMvc.perform(MockMvcRequestBuilders.post("/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jacksonObjectMapper().writeValueAsString(AddRequest(LINK))))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.key", Matchers.equalTo(KEY)))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.link", Matchers.equalTo(LINK)))
     }
 
 }
