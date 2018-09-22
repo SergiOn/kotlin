467a1dcd086d0f1d11014b2801cad778dbd3916b
SergiOn <onishhenko@gmail.com>
2018-09-22 10:26:30 +0300

    #server-side-development-spring-boot # section 2, lecture 5 # Creating Our First Spring Service

--- Server-Side-Development-with-Kotlin-and-Spring-Boot/demo_rest/src/main/kotlin/com/example/demo_rest/Controller.kt
+++ Server-Side-Development-with-Kotlin-and-Spring-Boot/demo_rest/src/main/kotlin/com/example/demo_rest/Controller.kt
@@ -1,11 +1,17 @@
 package com.example.demo_rest
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class Controller {
+
+//    val employeeService: EmployeeService = EmployeeService();
+
+    @Autowired
+    lateinit var employeeService: EmployeeService;
 
     @GetMapping
     fun getName(): String {
@@ -15,5 +21,10 @@ class Controller {
     @GetMapping("/{name}")
     fun getRealName(@PathVariable("name") name: String): String {
         return "Name: $name\n";
+    }
+
+    @GetMapping("/id/{id}")
+    fun getNameById(@PathVariable("id") id: Int): String {
+        return "${employeeService.findEmployee(id)}\n";
     }
 }
