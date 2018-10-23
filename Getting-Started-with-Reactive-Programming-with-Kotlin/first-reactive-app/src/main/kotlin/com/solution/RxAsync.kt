51a7240d786bd2e19d7212680a06a07027cc5396
SergiOn <onishhenko@gmail.com>
2018-10-23 23:16:10 +0300

    #reactive-programming # section 4, lecture 17 # Changing the Imperative try-catch Blocks

--- Getting-Started-with-Reactive-Programming-with-Kotlin/first-reactive-app/src/main/kotlin/com/solution/RxAsync.kt
+++ Getting-Started-with-Reactive-Programming-with-Kotlin/first-reactive-app/src/main/kotlin/com/solution/RxAsync.kt
@@ -1,10 +1,11 @@
 package com.solution
 
+import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.rxkotlin.toFlowable
-import io.reactivex.schedulers.Schedulers
 
 fun main(args: Array<String>) {
 
+    /*
     println("--- subscribe ---")
 
     listOf<String>("Alberto", "Peter", "Lara", "James").toFlowable()
@@ -37,5 +38,65 @@ fun main(args: Array<String>) {
             .doOnNext { println("Map operator was done on thread ${Thread.currentThread().name}") }
             .subscribeOn(Schedulers.newThread())
             .blockingSubscribe { println("Item $it was received on thread ${Thread.currentThread().name}") }
+
+*/
+
+    println("--- stream: Error ---")
+
+//    listOf<String>("Alberto", "Peter", "Lara", "Carl", "James").stream()
+//            .forEach {
+//                if (it == "Carl")
+//                    throw (IllegalArgumentException("Invalid"))
+//                else
+//                    println(it)
+//            }
+
+    listOf("Alberto", "Peter", "Lara", "Carl", "James").stream()
+            .forEach {
+                try {
+                    if (it == "Carl")
+                        throw (IllegalArgumentException("Invalid"))
+                    else
+                        println(it)
+                } catch (e: IllegalArgumentException) {
+                    println("We had an exception")
+                }
+            }
+
+    println("--- toFlowable: Error ---")
+
+    listOf("Alberto", "Peter", "Lara", "Carl", "James").toFlowable()
+            .doOnNext {
+                if (it == "Carl")
+                    throw (IllegalArgumentException("Invalid"))
+//                else
+//                    it
+            }
+            .subscribeBy(
+                    onNext = {
+                        println(it)
+                    },
+                    onError = {
+                        println("We had an exception")
+                    },
+                    onComplete = {
+                        println("Stream is complete")
+                    }
+            )
+
+    println("--- toFlowable: Complete ---")
+
+    listOf("Alberto", "Peter", "Lara", "Carl", "James").toFlowable()
+            .subscribeBy(
+                    onNext = {
+                        println(it)
+                    },
+                    onError = {
+                        println("We had an exception")
+                    },
+                    onComplete = {
+                        println("Stream is complete")
+                    }
+            )
 
 }
